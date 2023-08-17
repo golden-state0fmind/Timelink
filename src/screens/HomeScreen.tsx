02e0fd0edae5195ab0b1dd7e36cef0c0d338cdde
golden-state0fmind <areyes2009@yahoo.com>
2023-08-16 23:31:39 -0700

    adds description

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -14,6 +14,7 @@ const HomeScreen = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome Guest!</Text>
+            <Text style={styles.desc}>In our scheduling feature, you can effortlessly book appointments by selecting your preferred date and time slot from our intuitive calendar interface. Once you've chosen the perfect slot, you'll have the option to confirm your appointment through a convenient text message or email. This streamlined process ensures that you never miss out on important dates and can easily manage your schedule on the go.</Text>
             <NeumorphicButton title="Time to Thrive: Schedule Me!" onPress={goToDetails} />
         </View>
     );
@@ -24,10 +25,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 30,
+        backgroundColor:'#ffffff'
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
+        marginBottom: 24,
+    },
+    desc: {
+        fontSize: 16,
         marginBottom: 24,
     },
 });
