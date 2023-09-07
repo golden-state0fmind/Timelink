fd2078c92f275ffeb58842c518902e8cfae509c9
golden-state0fmind <areyes2009@yahoo.com>
2023-09-06 23:34:19 -0700

    moves styles to global style sheet

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+import { globalStyles } from '../assets/styles/globalStyles';
 import { useNavigation } from '@react-navigation/native';
 import NeumorphicButton from '../components/NeumorphicButton';
 
@@ -12,33 +13,13 @@ const HomeScreen = () => {
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Welcome Guest!</Text>
-            <Text style={styles.desc}>In our scheduling feature, you can effortlessly book appointments by selecting your preferred date and time slot from our intuitive calendar interface. Once you've chosen the perfect slot, you'll have the option to confirm your appointment through a convenient text message or email. This streamlined process ensures that you never miss out on important dates and can easily manage your schedule on the go.</Text>
-            <NeumorphicButton title="Time to Thrive: Schedule Me!" onPress={goToDetails} />
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.title}>Welcome Guest!</Text>
+            <Text style={globalStyles.desc}>In our scheduling feature, you can effortlessly book appointments by selecting your preferred date and time slot from our intuitive calendar interface. Once you've chosen the perfect slot, you'll have the option to confirm your appointment through a convenient text message or email. This streamlined process ensures that you never miss out on important dates and can easily manage your schedule on the go.</Text>
+            <NeumorphicButton title="Schedule Me!" onPress={goToDetails} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 30,
-        backgroundColor: '#E7D5C7',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 24,
-        color:'#073B4C'
-    },
-    desc: {
-        fontSize: 20,
-        marginBottom: 24,
-        color:'#073B4C'
-    },
-});
 
 export default HomeScreen;
