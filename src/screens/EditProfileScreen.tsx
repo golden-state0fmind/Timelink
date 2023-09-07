0c027dd875601efe92c01bdfa9d66140207c1f72
golden-state0fmind <areyes2009@yahoo.com>
2023-09-06 23:34:43 -0700

    adds global styles

--- src/screens/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { auth, signOut } from '../services/FirebaseUser';
 import { useNavigation } from '@react-navigation/native';
 import NeumorphicButton from '../components/NeumorphicButton';
+import { globalStyles } from '../assets/styles/globalStyles';
 
 const EditProfileScreen: React.FC = () => {
     const navigation: any = useNavigation();
@@ -14,32 +15,15 @@ const EditProfileScreen: React.FC = () => {
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Profile</Text>
-            <View style={styles.userContainer}>
-                <Text>Email: {user!.email}</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.title}>User</Text>
+            <View>
+                <Text style={globalStyles.desc} >Email: {user!.email}</Text>
             </View>
             <NeumorphicButton title="Sign Out" onPress={handleSignOut} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 32,
-        backgroundColor: '#f0f0f0', // Light gray background
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 24,
-    },
-    userContainer: {
-        marginBottom: 16,
-    },
-});
 
 export default EditProfileScreen;
